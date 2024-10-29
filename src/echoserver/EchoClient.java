d7dd88f6c0a97e871cb38cf8bfe83a9d781eac80
mahon479 <mahon479@morris.umn.edu>
2024-10-29 10:21:08 -0500

    Added code for the client and server. Bats tests are passing but I am running other tests now

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -17,7 +17,41 @@ public class EchoClient {
 		Socket socket = new Socket("localhost", PORT_NUMBER);
 		InputStream socketInputStream = socket.getInputStream();
 		OutputStream socketOutputStream = socket.getOutputStream();
-
-		// Put your code here.
+	
+		Thread inputThread = new Thread(() -> {
+			try {
+				int data;
+				while ((data = System.in.read()) != -1) {
+					socketOutputStream.write(data);
+				}
+				socket.shutdownOutput();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+	
+		Thread outputThread = new Thread(() -> {
+			try {
+				int data;
+				while ((data = socketInputStream.read()) != -1) {
+					System.out.write(data);
+				}
+				System.out.flush();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		});
+	
+		inputThread.start();
+		outputThread.start();
+	
+		try {
+			inputThread.join();
+			outputThread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	
+		socket.close();
 	}
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -7,25 +7,38 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-	
-	// REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
-	public static final int PORT_NUMBER = 0; 
-	public static void main(String[] args) throws IOException, InterruptedException {
-		EchoServer server = new EchoServer();
-		server.start();
-	}
+    
+    // REPLACE WITH PORT PROVIDED BY THE INSTRUCTOR
+    public static final int PORT_NUMBER = 6013; // Example port number
 
-	private void start() throws IOException, InterruptedException {
-		ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
-		while (true) {
-			Socket socket = serverSocket.accept();
+    public static void main(String[] args) throws IOException, InterruptedException {
+        EchoServer server = new EchoServer();
+        server.start();
+    }
 
-			// Put your code here.
-			// This should do very little, essentially:
-			//   * Construct an instance of your runnable class
-			//   * Construct a Thread with your runnable
-			//      * Or use a thread pool
-			//   * Start that thread
-		}
-	}
+    private void start() throws IOException, InterruptedException {
+        ServerSocket serverSocket = new ServerSocket(PORT_NUMBER);
+        while (true) {
+            Socket socket = serverSocket.accept();
+
+            // Create and start a new thread for each client connection
+            Thread clientThread = new Thread(() -> {
+                try {
+                    InputStream inputStream = socket.getInputStream();
+                    OutputStream outputStream = socket.getOutputStream();
+
+                    int data;
+                    while ((data = inputStream.read()) != -1) {
+                        outputStream.write(data);
+                    }
+
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+
+            clientThread.start();
+        }
+    }
 }
